3af23cb2724ee8a112e7ad5d628339e5f057c1cb
rtthw <172864013+rtthw@users.noreply.github.com>
2025-02-19 10:18:16 -0500

    Implement more complex environment functionality

--- src/env.rs
+++ src/env.rs
@@ -2,17 +2,44 @@
 
 
 
-use std::sync::{Arc, Mutex};
-
-
-
 #[derive(Clone)]
-pub struct Environment(Arc<Mutex<EnvironmentImpl>>);
+pub struct Environment(std::sync::Arc<parking_lot::RwLock<EnvironmentImpl>>);
 
 impl Default for Environment {
     fn default() -> Self {
-        Self(Arc::new(Mutex::new(EnvironmentImpl {  })))
+        Self(std::sync::Arc::new(parking_lot::RwLock::new(EnvironmentImpl {
+            manifests: vec![
+                calculator_manifest(),
+                manual_manifest(),
+            ],
+        })))
     }
 }
 
-struct EnvironmentImpl {}
+impl Environment {
+    fn read<R>(&self, reader: impl FnOnce(&EnvironmentImpl) -> R) -> R {
+        reader(&self.0.read())
+    }
+
+    fn write<R>(&self, writer: impl FnOnce(&mut EnvironmentImpl) -> R) -> R {
+        writer(&mut self.0.write())
+    }
+}
+
+struct EnvironmentImpl {
+    manifests: Vec<Manifest>,
+}
+
+
+
+pub struct Manifest {
+    name: &'static str, // All manifests are hard-coded.
+}
+
+fn calculator_manifest() -> Manifest {
+    Manifest { name: "Calculator" }
+}
+
+fn manual_manifest() -> Manifest {
+    Manifest { name: "Manual" }
+}
