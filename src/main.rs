cd155c7a55de95c57f375fea8efc979b17c257b2
rtthw <172864013+rtthw@users.noreply.github.com>
2025-04-07 11:07:47 -0400

    Setup WASM binary

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,38 @@
+#![recursion_limit = "1024"]
 
 
 
-fn main() {}
+use wasm_bindgen::prelude::*;
+use web_sys::window;
+
+
+
+fn main() {
+    console_error_panic_hook::set_once();
+    start_app();
+}
+
+fn start_app() {
+    let document = document();
+    let body = body(&document);
+    let text_node = document.create_text_node("Testing, testing...");
+    body.append_child(text_node.as_ref())
+        .expect("failed to append text");
+}
+
+#[wasm_bindgen]
+pub fn wasm_ffi() {
+    web_sys::console::log_1(&"Something, something, something...".into());
+}
+
+
+
+fn document() -> web_sys::Document {
+    window()
+        .and_then(|win| win.document())
+        .expect("could not access document")
+}
+
+fn body(doc: &web_sys::Document) -> web_sys::HtmlElement {
+    doc.body().expect("could not access document.body")
+}
